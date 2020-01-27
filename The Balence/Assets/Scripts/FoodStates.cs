45279652fe03c583b55977999f20307fbab3ab1d
AAYF <samuel.hawaii@gmail.com>
2020-01-27 06:54:21 -0600

    Food
    food level

---
+++ The Balence/Assets/Scripts/FoodStates.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodStates : MonoBehaviour
+{
+    public int currentFoodState;
+    public GameObject Win;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentFoodState = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentFoodState == 3)
+        {
+            Win.SetActive(true);
+        }
+    }
+
+    public void advanceState()
+    {
+        currentFoodState += 1;
+    }
+}
---
+++ The Balence/Assets/Scripts/FoodTimer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodTimer : MonoBehaviour
+{
+    public GameObject fillbar;
+    public Image bar;
+    public float filltimer;
+    public bool isfilling;
+    private float timer;
+    FoodStates foodstate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isfilling = false;
+        
+        foodstate = GameObject.FindGameObjectWithTag("GameManager").GetComponent<FoodStates>();
+        fillbar.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isfilling == true)
+        {
+            timer += 1 * Time.deltaTime;
+        }
+        bar.fillAmount = (timer / filltimer);
+
+
+        if(timer >= filltimer)
+        {
+            isfilling = false;
+            timer = 0;
+            foodstate.advanceState();
+            fillbar.SetActive(false);
+            
+            
+        }
+    }
+
+
+
+    public void startfilling()
+    {
+        fillbar.SetActive(true);
+        isfilling = true;
+
+    }
+
+    public void stopFilling()
+    {
+        fillbar.SetActive(false);
+        isfilling = false;
+        timer = 0;
+    }
+
+
+}
