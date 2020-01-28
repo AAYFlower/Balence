ff0c78770e48ed3b3e4a9a61a128e607fc256930
AAYF <samuel.hawaii@gmail.com>
2020-01-27 21:52:09 -0600

    Shark

---
+++ The Balence/Assets/SharkBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkBehavior : MonoBehaviour
+{
+
+    public float[] forceRange;
+    public float timer;
+    private float time;
+    int randomRotate;
+    Rigidbody2D rb;
+    bool random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        random = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(random == true)
+        {
+            randomRotate = 0;
+        }
+
+        if(random == false)
+        {
+            randomRotate = 1;
+        }
+        time += 1 * Time.deltaTime;
+
+        if(time >= timer)
+        {
+
+            rb.AddTorque(forceRange[randomRotate]);
+            random = !random;
+            time = 0;
+        }
+
+        
+
+        
+    }
+}
