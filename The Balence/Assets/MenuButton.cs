6bbb16f84f4135c62aa35a937f1fb1a042666b3d
AAYF <samuel.hawaii@gmail.com>
2020-02-24 19:25:37 -0600

    Uploaded Backup

---
+++ The Balence/Assets/MenuButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public Animator anim;
+    bool menuOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleMenu()
+    {
+        menuOpen = !menuOpen;
+        anim.SetBool("Menu", menuOpen);
+    }
+}
