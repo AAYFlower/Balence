5cbfe38c4b4b8f4a0bed48801c62160858eba552
AAYF <samuel.hawaii@gmail.com>
2020-03-02 21:41:47 -0600

    a

---
+++ The Balence/Assets/CameraSizeIncrease.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSizeIncrease : MonoBehaviour
+{
+    Camera maincamera;
+    public float IncreaseRate;
+    public float maxCameraSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maincamera.orthographicSize <= maxCameraSize)
+        {
+            maincamera.orthographicSize += IncreaseRate * Time.deltaTime;
+        }
+    }
+}
